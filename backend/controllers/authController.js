d98d50ebe0dbfb603b2449bef56cdbc784f3cf26
Argeem <sitthisakbannob@gmail.com>
2026-03-11 21:44:00 +0700

    Prepare Project Structure

---
+++ backend/controllers/authController.js
@@ -0,0 +1,14 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register User
+const registerUser = async (req, res) => {};
+
+// Login User
+const loginUser = async (req, res) => {};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
