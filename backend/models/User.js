64019540c673ad4a6641bd8d6354886737cb8fcd
Argeem <sitthisakbannob@gmail.com>
2026-03-16 20:47:40 +0700

    Update User Schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,13 +29,16 @@ const UserSchema = new mongoose.Schema(
 );
 
 // Encrypt password before saving
-UserSchema.pre("save", async function (next) {
+UserSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", UserSchema);
 
